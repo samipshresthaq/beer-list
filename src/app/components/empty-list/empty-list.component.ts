34daae8d72b7377d27b63cfa47aa64d52a719177
Samip Shrestha <javasamip@gmail.com>
2023-05-03 02:26:29 +0545

    feat: empty list component added

---
+++ src/app/components/empty-list/empty-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-list',
+  templateUrl: './empty-list.component.html',
+  styleUrls: ['./empty-list.component.scss'],
+})
+export class EmptyListComponent {}
