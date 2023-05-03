67f52e1acf04ea2410fe71b65cb92862ec031dc3
Samip Shrestha <javasamip@gmail.com>
2023-05-03 22:57:37 +0545

    fix: design and performance

--- src/app/components/item-list/item-list.component.ts
+++ src/app/components/item-list/item-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { BeerInterface } from 'src/app/models/beer.interface';
 
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
   styleUrls: ['./item-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ItemListComponent {
   @Input() items: BeerInterface[] = [];
