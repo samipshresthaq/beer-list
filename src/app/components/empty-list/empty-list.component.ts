380f5db231c934a53526778057c14850d1066959
Samip Shrestha <javasamip@gmail.com>
2023-05-04 00:06:48 +0545

    design: empty list

--- src/app/components/empty-list/empty-list.component.ts
+++ src/app/components/empty-list/empty-list.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-empty-list',
   templateUrl: './empty-list.component.html',
   styleUrls: ['./empty-list.component.scss'],
 })
-export class EmptyListComponent {}
+export class EmptyListComponent {
+  @Output() public addItem = new EventEmitter<void>();
+
+  onAdd() {
+    this.addItem.emit();
+  }
+}
