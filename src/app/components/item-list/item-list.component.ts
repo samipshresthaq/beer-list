0913a635e7172bc1db757cb73e835a42e00c02cb
Samip Shrestha <javasamip@gmail.com>
2023-05-05 21:06:12 +0545

    chore: code refactor

--- src/app/components/item-list/item-list.component.ts
+++ src/app/components/item-list/item-list.component.ts
@@ -1,5 +1,8 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { BeerInterface } from 'src/app/models/beer.interface';
+import {
+  BeerInterface,
+  UserBeerInterface,
+} from 'src/app/models/beer.interface';
 
 @Component({
   selector: 'app-item-list',
@@ -8,9 +11,9 @@ import { BeerInterface } from 'src/app/models/beer.interface';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ItemListComponent {
-  @Input() items: BeerInterface[] = [];
+  @Input() items: Array<BeerInterface | UserBeerInterface> = [];
 
-  trackByItems(index: number, item: BeerInterface): number {
-    return item.id;
+  trackByItems(index: number, item: BeerInterface | UserBeerInterface): number {
+    return index;
   }
 }
--- src/app/pipes/ingredients.pipe.ts
+++ src/app/pipes/ingredients.pipe.ts
@@ -8,7 +8,7 @@ import { BeerIngredients } from '../models/beer.interface';
 export class IngredientsPipe implements PipeTransform {
   transform(value: { [type: string]: Array<BeerIngredients> }, ...args: any[]) {
     const preText = 'Ingredients:';
-    if (Object.keys(value).length) {
+    if (value && Object.keys(value).length) {
       return `${preText} ${Object.keys(value).join(', ')}`;
     }
 
