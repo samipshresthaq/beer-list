194d5a2b2e338469cdda126c20c926ad6a784f90
Samip Shrestha <javasamip@gmail.com>
2023-05-05 17:04:57 +0545

    feat: added logger service

---
+++ src/app/services/logger.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoggerService {
+  log(message: string) {
+    console.log(message);
+  }
+  error(message: string) {
+    console.error(message);
+  }
+  warn(message: string) {
+    console.warn(message);
+  }
+  info(message: string) {
+    console.info(message);
+  }
+}
